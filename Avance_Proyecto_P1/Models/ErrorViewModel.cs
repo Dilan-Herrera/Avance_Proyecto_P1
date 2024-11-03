3f1317a01a110364d632e81efafd51b60ed8d206
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2024-11-03 15:03:08 -0500

    Agregar archivos de proyecto.

---
+++ Avance_Proyecto_P1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Avance_Proyecto_P1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
